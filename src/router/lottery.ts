387871ec1a3787e7b15eb720fc5a6ddbe5a2a2b7
phongbugs <nguyenduyphongtn@gmail.com>
2024-10-31 14:31:07 +0700

    add lottery router to decrease components on vue app
    - The slow performance is caused by the Vue DevTools (for Vue v3 only). Turning off this extension in Chrome will make the page load faster immediately.

---
+++ src/router/lottery.ts
@@ -0,0 +1,57 @@
+import {
+  createRouter,
+  createWebHashHistory,
+  type RouteRecordRaw,
+} from "vue-router";
+import { useAuthStore } from "@/stores/auth";
+import { useConfigStore } from "@/stores/config";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/lottery",
+    component: () => import("@/components/lottery/Power655.vue"),
+    meta: {
+      pageTitle: "POMELO",
+    },
+    children: [],
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore();
+  const configStore = useConfigStore();
+
+  // current page view title
+  document.title = `${to.meta.pageTitle} - ${import.meta.env.VITE_APP_NAME}`;
+
+  // reset config to initial state
+  configStore.resetLayoutConfig();
+
+  // verify auth token before each page change
+  authStore.verifyAuth();
+
+  // before page access check if page requires authentication
+  if (to.meta.middleware == "auth") {
+    if (authStore.isAuthenticated) {
+      next();
+    } else {
+      next({ name: "sign-in" });
+    }
+  } else {
+    next();
+  }
+
+  // Scroll page to top on every route change
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+export default router;
