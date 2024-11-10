71e7bbced196d7240415290fda5e8bbbea875bcd
phongbugs <nguyenduyphongtn@gmail.com>
2024-11-10 12:57:13 +0700

    fix _dirname variable error

--- api/src/handlers/Power655DrawHandler.ts
+++ api/src/handlers/Power655DrawHandler.ts
@@ -2,6 +2,11 @@
 import * as cheerio from "cheerio";
 import * as fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 import { Power655Draw } from "@prisma/client";
 
 export class DrawPower655Handler {
